22602ebdf08d0984a245c817a5ed4d15ff164c83
You are source code <58625635+youaresourcecode@users.noreply.github.com>
2020-04-04 10:53:55 +0200

    Update ambientterminal.sh
    Eliminada línea con comentario.

--- ambientterminal.sh
+++ ambientterminal.sh
@@ -2,6 +2,5 @@
 #terminalente = música ambiente en el terminal 
 clear
 echo Mezcla de "$1" y "$2"
-#ffmpeg -i heavy-rain.mp3 -i thunderstorm.mp3 -filter_complex amix=inputs=2:duration=longestoutput.mp3 #No funciona
 ffmpeg -i "$1" -i "$2" -filter_complex amerge=inputs=2 -ac 2 output.mp3
 afplay output.mp3 
